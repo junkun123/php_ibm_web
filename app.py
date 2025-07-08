e6b48d7fa12184469798a9b810cf723f278bf652
JUN <jun@jun.JUN>
2025-07-07 20:46:30 -0600

    Conexión a DB2 por ngrok 2.tcp.ngrok.io:19345

--- app.py
+++ app.py
@@ -1,51 +1,26 @@
 from flask import Flask
-
 import ibm_db
-
-
 
 app = Flask(__name__)
 
-
-
 @app.route('/')
-
 def conectar_db2():
-
     conn_str = (
-
         "DATABASE=BIBLIO;"
-
         "HOSTNAME=2.tcp.ngrok.io;"
-
         "PORT=19345;"
-
         "PROTOCOL=TCPIP;"
-
         "UID=db2inst1;"
-
         "PWD=juan22;"
-
     )
-
     try:
-
         conn = ibm_db.connect(conn_str, '', '')
-
         if conn:
-
             return "✅ Conexión exitosa a DB2"
-
         else:
-
             return "❌ No se pudo conectar a DB2"
-
     except Exception as e:
-
         return f"❌ Error de conexión: {e}"
 
-
-
 if __name__ == '__main__':
-
     app.run(host='0.0.0.0', port=8080)
